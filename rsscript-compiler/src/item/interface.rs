5ac3a47c836bd5c9f4222d60b1dd75f7d0b63f7a
kimuti-tsukai <ruster354@gmail.com>
2024-12-17 13:39:45 +0900

    Impl Parse for InterfaceItemFn

--- rsscript-compiler/src/item/interface.rs
+++ rsscript-compiler/src/item/interface.rs
@@ -1,8 +1,4 @@
-use syn::{
-    punctuated::Punctuated,
-    token::{Brace, Paren},
-    Ident, Type, TypeParamBound,
-};
+use syn::{parse::Parse, punctuated::Punctuated, token::Brace, Ident, Type, TypeParamBound};
 
 use crate::{generics::Generics, restrinction::Visibility, stmt::Block, Token};
 
@@ -27,11 +23,41 @@ pub struct InterfaceItemFn {
     pub function_token: Token![function],
     pub ident: Ident,
     pub generics: Generics,
-    pub paren_token: Paren,
     pub inputs: FnArgs,
     pub output: Option<TypeAnnotation>,
     pub default: Option<Block>,
     pub semicolon_token: Option<Token![;]>,
+}
+
+impl Parse for InterfaceItemFn {
+    fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
+        let function_token = input.parse()?;
+        let ident = input.parse()?;
+        let generics = input.parse()?;
+        let inputs = input.parse()?;
+        let output = if input.peek(Token![=]) {
+            Some(input.parse()?)
+        } else {
+            None
+        };
+        let (default, semicolon_token) = if input.peek(Brace) {
+            (Some(input.parse()?), None)
+        } else if input.peek(Token![;]) {
+            (None, Some(input.parse()?))
+        } else {
+            (None, None)
+        };
+
+        Ok(Self {
+            function_token,
+            ident,
+            generics,
+            inputs,
+            output,
+            default,
+            semicolon_token,
+        })
+    }
 }
 
 pub struct InterfaceItemType {
